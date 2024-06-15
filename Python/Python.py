eff205432f6e869c17caeeb1278604c9cb1ebae0
guisaan <gsantoroesilva@gmail.com>
2024-06-14 21:53:24 -0300

    Arquivos a serem enviados

--- Python/Python.py
+++ Python/Python.py
@@ -6,4 +6,4 @@ No caso do código será o que desejamos executar das estapas.
 
 1. Entrada de Dados - os possiveis dados que devem ser lidos antes da rotina programada
 2. Processamento de Dados - executar as ações necessária para chegar na saída.
-3. Saída de dados - resultado final da progrmação.
+3. Saída de dados - resultado final da programação.
