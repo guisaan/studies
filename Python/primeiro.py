4b3a341f78a3931760b5b993f4e3efb4e9b39c92
guisaan <gsantoroesilva@gmail.com>
2024-06-14 21:19:25 -0300

    Arquivos adicionados para dar continuidade nos estudos

---
+++ Python/primeiro.py
@@ -0,0 +1,4 @@
+# função para exibir informção na tela
+print ('Hellow, world!')
+# função que permite criar uma ideia de inserção de dados
+input ('Qual é a linguagem de programação que você está estudando?')
