9ffdca4b06e1f07e30f990e046b86f71275e4886
Gui_Santoro <guisaan@live.com>
2025-02-14 16:47:08 -0300

    Delete Python directory

--- Python/Python.py
+++
@@ -1,9 +0,0 @@
-##Python é uma linguagem de programação generalista, multifunções;
-Agora vamos aprender os fudamentos da linguagem
-
-Sempre que desenvolvemos um codigo, ele é baseado em algum algoritmo. Como uma receita de bolo
-No caso do código será o que desejamos executar das estapas.
-
-1. Entrada de Dados - os possiveis dados que devem ser lidos antes da rotina programada
-2. Processamento de Dados - executar as ações necessária para chegar na saída.
-3. Saída de dados - resultado final da programação.
--- Python/primeiro.py
+++
@@ -1,4 +0,0 @@
-# função para exibir informação na tela
-print ('Hellow, world!')
-# função que permite criar uma ideia de inserção de dados
-input ('Qual é a linguagem de programação que você está estudando?')
